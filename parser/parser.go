e37099ef131411288bfa8e8347b9cb7f6f1dc792
ningnsm9 <ningnsm9@gmail.com>
2025-03-23 08:18:10 +0000

    hello

--- parser/parser.go
+++ parser/parser.go
@@ -86,6 +86,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -8,13 +8,14 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-	let x = 5;
-	let y = 10;
-	let foobar = 838383;
+	let x 5;
+	let = 10;
+	let 838383;
 	`
 	l := lexer.New(input)
 	p := New(l)
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -44,15 +45,15 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 	letStmt, ok := s.(*ast.LetStatement)
 	if !ok {
-		t.Errorf("s not *ast.LetStatement. got %T", s)
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
 		return false
 	}
 	if letStmt.Name.Value != name {
-		t.Errorf("letStmt.Name.Value not '%s'. got %s", name, letStmt.Name.Value)
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
 		return false
 	}
 	if letStmt.Name.TokenLiteral() != name {
-		t.Errorf("s.Name not '%s'. got %s", name, letStmt.Name)
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
 		return false
 	}
 	return true
